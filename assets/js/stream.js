6aa53a9631d8b149362df4f05aeda4dd3dd4cc75
phdenzel <phdenzel@gmail.com>
2018-08-03 09:23:04 +0200

    Added test case

---
+++ assets/js/stream.js
@@ -0,0 +1,31 @@
+// Grabbing settings
+var ww = window.innerWidth;
+var wh = window.innerHeight;
+var video = document.getElementById('video');
+var dst = document.getElementById('dst');
+
+
+function initSuccess() {
+    console.log('CamEngine starting...');
+    CamEngine.start();
+}
+
+
+function alertError(error) {
+    console.log(error);
+    alert('Error while accessing user media' + error.toString());
+}
+
+
+CamEngine.init({
+    video: video,
+    captureWidth: 1980,
+    captureHeight: 1080,
+    procWidth: ww,
+    procHeight: wh,
+    outputCvs: dst,
+    doProcess: false,
+    processor: function() {},
+    initSuccessCallback: initSuccess,
+    initErrorCallback: alertError
+});
